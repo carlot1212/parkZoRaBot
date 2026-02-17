1559ad7a865112345f590592f0daa934429fc2b8
Carlo Torres <carlotorres1212@gmail.com>
2026-02-16 20:22:39 -0800

    groceries

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from dotenv import load_dotenv
 import os
 import webserver
 from string import Template
+
 load_dotenv()
 
 token = os.getenv('DISCORD_API_KEY')
@@ -98,6 +99,48 @@ async def harmonies(ctx):
     await scoring_thread.send(scoring_message)
 
     await thread.delete()
+
+@bot.command()
+async def add(ctx, *groceries):
+   if ctx.channel.name != 'groceries':
+      return
+   
+   groceries = [grocery.strip(',') for grocery in groceries]
+
+   grocery_thread = discord.utils.get(ctx.channel.threads, name="Grocery List")
+
+   if not grocery_thread:
+      grocery_thread = await ctx.channel.create_thread(name="Grocery List") 
+      for member in ctx.channel.members:
+         await grocery_thread.add_user(member)
+
+   for grocery in groceries:
+       await grocery_thread.send(grocery)
+
+@bot.command()
+async def remove(ctx, *groceries): 
+    if ctx.channel.name != 'groceries':
+        return
+    
+    groceries = [grocery.strip(',') for grocery in groceries]
+
+    grocery_thread = discord.utils.get(ctx.channel.threads, name="Grocery List")
+
+    async for message in grocery_thread.history(limit=100):
+        if message.content in groceries:
+            await message.delete()
+
+@bot.command()
+async def done(ctx):
+    if ctx.channel.name != 'Grocery List':
+        return
+    
+    await ctx.channel.edit(archived=True)
+
+    groceries_channel = discord.utils.get(ctx.guild.channels, name='groceries')
+
+    async for message in groceries_channel.history(limit=100):
+        await message.add_reaction('✅')
 
 webserver.keep_alive()
 bot.run(token, log_handler=handler, log_level=logging.DEBUG)
