bc3390d3d338df53768131755637b202411b47a6
Carlo Torres <carlotorres1212@gmail.com>
2026-04-06 17:12:54 -0700

    server fix?

--- webserver.py
+++ webserver.py
@@ -1,14 +1,17 @@
 from flask import Flask
 from threading import Thread
+import os
 
 app = Flask('')
+
 @app.route('/')
 def home():
     return 'Discord Bot is running!'
 
 def run():
-    app.run(host='0.0.0.0', port=8080)
+    port = int(os.getenv('PORT', 8080))  # Render sets PORT env var
+    app.run(host='0.0.0.0', port=port)
 
 def keep_alive():
-    t = Thread(target=run)
+    t = Thread(target=run, daemon=True)
     t.start()
